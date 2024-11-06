6049e9e15110d9309d4216da45e83f70c7e4c903
zetski <batuigasjerichodesu@gmail.com>
2024-11-06 19:26:57 +0800

    save

--- admin/login.php
+++ admin/login.php
@@ -182,15 +182,15 @@ $('#toggle-password').on('click', function() {
     }
 });
 
-Disable inspect element and right-click
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function(e) {
-    if (e.keyCode == 123 || e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0) || 
-        e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0) || 
-        e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-};
+// Disable inspect element and right-click
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.onkeydown = function(e) {
+//     if (e.keyCode == 123 || e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0) || 
+//         e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0) || 
+//         e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//         return false;
+//     }
+// };
   </script>
 </body>
 </html>
