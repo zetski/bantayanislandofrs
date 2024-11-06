180441241e526579b64f4a7865a6b85074517a20
zetski <batuigasjerichodesu@gmail.com>
2024-11-07 07:30:16 +0800

    save

--- upcoming_events.php
+++ upcoming_events.php
@@ -25,7 +25,6 @@
         }
         .event-item {
             min-width: 100%;
-            height: 100%;
             background-color: white;
             display: flex;
             flex-direction: column;
