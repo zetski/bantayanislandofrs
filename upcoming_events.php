530f7335c535ad0d07087c6c2db40ca8fb4e5a84
zetski <batuigasjerichodesu@gmail.com>
2024-11-07 07:27:47 +0800

    save

--- upcoming_events.php
+++ upcoming_events.php
@@ -33,7 +33,7 @@
         }
         .event-item img {
             width: 100%;
-            height: 60%;
+            height: 30%;
             object-fit: cover;
         }
         .event-details {
