dd83a665e6c7fc1d17a2f3066035254a4122152f
zetski <batuigasjerichodesu@gmail.com>
2024-11-07 21:57:09 +0800

    save

--- report/index.php
+++ report/index.php
@@ -1,18 +1,18 @@
 <?php
-// Secure Headers
-header("Content-Security-Policy: default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self';");
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: SAMEORIGIN");
-header("X-XSS-Protection: 1; mode=block");
-header("Referrer-Policy: no-referrer-when-downgrade");
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+// // Secure Headers
+// header("Content-Security-Policy: default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self';");
+// header("X-Content-Type-Options: nosniff");
+// header("X-Frame-Options: SAMEORIGIN");
+// header("X-XSS-Protection: 1; mode=block");
+// header("Referrer-Policy: no-referrer-when-downgrade");
+// header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
 
-// Secure Cookie Flags
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.use_only_cookies', 1);
+// // Secure Cookie Flags
+// ini_set('session.cookie_httponly', 1);
+// ini_set('session.cookie_secure', 1);
+// ini_set('session.use_only_cookies', 1);
 
-session_start();
+// session_start();
 
 // Define a function to sanitize input data
 function sanitizeInput($data) {
