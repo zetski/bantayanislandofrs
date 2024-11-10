d49060d27003f225255a6d300a47351c632a05fc
zetski <batuigasjerichodesu@gmail.com>
2024-11-10 18:35:46 +0800

    save

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -104,7 +104,7 @@
   }
 </style>
 
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: gray;">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #ff4600;">
   <div class="container px-4 px-lg-5">
     <a class="navbar-brand" href="./">
       <img src="<?php echo validate_image($_settings->info('logo')) ?>" width="30" height="30" alt="Logo" loading="lazy">
