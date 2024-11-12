75ae16a5e1db8dcab9a5a897f5926c87849869bc
zetski <batuigasjerichodesu@gmail.com>
2024-11-12 11:40:28 +0800

    save

--- admin_login.php
+++ admin_login.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('config.php'); // Ensure your DB connection is available here
+require_once('../initialize.php'); // Ensure your DB connection is available here
 
 function is_admin_email($email) {
     global $conn;
