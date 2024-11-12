ae6c33fb3f2dd45deeff921d637cc3f20842aaf5
zetski <batuigasjerichodesu@gmail.com>
2024-11-12 12:48:28 +0800

    save

--- verify_email.php
+++ verify_email.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('../config.php'); // Ensure the config file path is correct
+require_once('config.php'); // Ensure the config file path is correct
 
 session_start();
 
