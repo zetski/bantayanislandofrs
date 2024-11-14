4d8b48d00f53ecd41e8728851f0f7d2086596560
zetski <batuigasjerichodesu@gmail.com>
2024-11-14 08:58:39 +0800

    save

--- verify_email.php
+++ verify_email.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             border-radius: 8px;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
             background-color: #fff;
-            max-width: 500px;
+            max-width: 400px;
             width: 90%;
         }
         h2 {
