fd2b2b956b6707bb21f60a9d05eabfef8f37a3ae
zetski <batuigasjerichodesu@gmail.com>
2024-11-15 19:56:03 +0800

    save

--- home.php
+++ home.php
@@ -23,7 +23,7 @@
 <section class="py-3">
     <div class="container">
         <div class="row">
-            <div class="col-xl-12">
+            <div class="col-md-12">
                 <div id="carouselExampleControls" class="carousel slide bg-dark" data-ride="carousel">
                     <div class="carousel-inner">
                         <?php 
