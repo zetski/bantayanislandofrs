227c285e0354785d646b02a3f3638c46ad5161ef
zetski <batuigasjerichodesu@gmail.com>
2024-11-15 21:01:29 +0800

    s

--- home.php
+++ home.php
@@ -63,9 +63,7 @@
                             </center>
                             <?= htmlspecialchars_decode(file_get_contents('./welcome.html')) ?>
                             <div>
-                                <center>
                                 <button class="btn" onclick="window.location.href='./upcoming_events.php';">Upcoming Events</button>
-                                </center>
                             </div>
                         </div>
                     </div>
