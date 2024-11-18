d6d8fc5e9828e675523085952fdb39ac185ba140
zetski <batuigasjerichodesu@gmail.com>
2024-11-18 19:14:53 +0800

    sa

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@
     <?php endif;?>      
      <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
       <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper  pt-3" style="min-height: 567.854px;">
+      <div class="content-wrapper  pt-3" style="min-height: 567.854px; min-width: 320px">
      
         <!-- Main content -->
         <section class="content  text-dark">
