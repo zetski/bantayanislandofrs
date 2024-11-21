57deed60fcfb61f247311202db74b686f837b973
zetski <batuigasjerichodesu@gmail.com>
2024-11-21 08:24:01 +0800

    sa

--- admin/user/list.php
+++ admin/user/list.php
@@ -80,9 +80,9 @@
 				                  		Action
 				                    <span class="sr-only">Toggle Dropdown</span>
 				                  </button>
-				                  <!-- <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item" href="./?page=user/manage_user&id=</?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
-				                    <div class="dropdown-divider"></div> -->
+				                  <div class="dropdown-menu" role="menu">
+				                    <a class="dropdown-item" href="./?page=user/manage_user&id=<?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
+				                    <div class="dropdown-divider"></div>
 				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
 				                  </div>
 							</td>
