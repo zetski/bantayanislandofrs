2bf1e97e23fa65d83c103f83ecc7604231d91222
zetski <batuigasjerichodesu@gmail.com>
2024-11-27 08:30:34 +0800

    save

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -3,8 +3,7 @@
 		alert_toast("<?php echo $_settings->flashdata('success') ?>",'success');
 	</script>
 	<?php endif;?>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 	<style>
 		img#cimg{
 			height: 15vh;
