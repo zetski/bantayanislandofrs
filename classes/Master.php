7e49dcf1c0e55794de10e9dc2ffe169ed85ba772
zetski <batuigasjerichodesu@gmail.com>
2024-11-28 10:53:06 +0800

    save

--- classes/Master.php
+++ classes/Master.php
@@ -531,6 +531,22 @@ Class Master extends DBConnection {
 		echo json_encode(['status' => 'success', 'officers' => $officers]);
 	}
 
+	public function get_officer() {
+		extract($_POST);
+		$sql = "SELECT id, lastname, firstname, middlename, position, image FROM officers WHERE id = ?";
+		$stmt = $this->conn->prepare($sql);
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if ($result->num_rows > 0) {
+			$officer = $result->fetch_assoc();
+			$officer['image'] = validate_image($officer['image']); // Validate image URL
+			echo json_encode(['status' => 'success', 'officer' => $officer]);
+		} else {
+			echo json_encode(['status' => 'failed', 'error' => 'Officer not found.']);
+		}
+	}
+
 	function save_inquiry(){
 		$_POST['message'] = addslashes(htmlspecialchars($_POST['message']));
 		extract($_POST);
@@ -589,6 +605,9 @@ switch ($action) {
         break;
 	case 'get_officers':
 		echo $Master->get_officers();
+		break;
+	case 'get_officer':
+		echo $Master->get_officer();
 		break;
 	case 'delete_img':
 		echo $Master->delete_img();
