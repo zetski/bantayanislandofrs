14a2bb42b8d98e0aa95ff86a3cb32d9295e5c395
zetski <batuigasjerichodesu@gmail.com>
2024-11-28 16:48:36 +0800

    save

--- choose_access.php
+++ choose_access.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['role'])) {
         header("Location: ./index");
         exit;
     } elseif ($_SESSION['role'] === 'admin') {
-        header("Location: ../forgot/verify_email.php");
+        header("Location: ./verify_email");
         exit;
     }
 }
@@ -56,7 +56,7 @@ if (isset($_SESSION['role'])) {
     <div class="container">
         <h2>Welcome to the Bantayan BFP</h2>
         <button class="btn guest-btn" onclick="window.location.href='./set_guest'">Continue as Guest</button>
-        <button class="btn admin-btn" onclick="window.location.href='forgot/verify_email.php'">Login as Admin</button>
+        <button class="btn admin-btn" onclick="window.location.href='./admin'">Login as Admin</button>
     </div>
 </body>
 </html>
