b8ac66c2e7a303c2fb9e505e8d028eb40153d498
zetski <batuigasjerichodesu@gmail.com>
2024-11-28 16:03:05 +0800

    save

--- classes/Login.php
+++ classes/Login.php
@@ -65,7 +65,7 @@ class Login extends DBConnection {
 	}
 	public function logout(){
 		if($this->settings->sess_des()){
-			redirect('admin/login.php');
+			redirect('admin/login');
 		}
 	}
 	function login_user(){
