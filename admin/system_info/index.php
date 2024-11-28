092a626e5752f8cbd01f38590dbfeb8adf669785
zetski <batuigasjerichodesu@gmail.com>
2024-11-28 10:48:21 +0800

    save

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -282,10 +282,20 @@
 										<td>${officer.position}</td>
 										<td><img src="${officer.image}" alt="Officer Image" class="img-thumbnail" width="50" height="50"></td>
 										<td>
-											<button class="btn btn-sm btn-danger" onclick="delete_officer(${officer.id})">
-												<i class="fa fa-trash"></i> Delete
+										<div class="btn-group">
+											<button type="button" class="btn btn-sm btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												Actions
 											</button>
-										</td>
+											<div class="dropdown-menu">
+												<a class="dropdown-item" href="#" onclick="edit_officer(${officer.id})">
+													<i class="fa fa-edit"></i> Edit
+												</a>
+												<a class="dropdown-item text-danger" href="#" onclick="delete_officer(${officer.id})">
+													<i class="fa fa-trash"></i> Delete
+												</a>
+											</div>
+										</div>
+									</td>
 									</tr>
 								`;
 							});
