d74c07ac139699ba66736eb3a2c57061c9fc0f39
zetski <batuigasjerichodesu@gmail.com>
2024-11-28 18:54:16 +0800

    save

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -265,24 +265,22 @@
 								showConfirmButton: true // Ensure the button is visible
 							}).then(() => {
 								$('#officers-frm')[0].reset();
-								$('#officer-images-preview').html('');
 								loadOfficers(); // Reset the form
 
 								// Dynamically add the new officer to the table
-								var newRow = `
-								<tr>
-									<td>${response.id}</td>
-									<td>${response.lastname} ${response.firstname} ${response.middlename}</td>
-									<td>${response.position}</td>
-									<td><img src="${response.image}" alt="Officer Image" class="img-thumbnail" style="width: 50px; height: 50px;"></td>
-									<td>
-										<button class="btn btn-sm btn-danger" onclick="delete_officer(${response.id})">
-											<i class="fa fa-trash"></i> Delete
-										</button>
-									</td>
-								</tr>
-							`;
-							$('#officers-table tbody').append(newRow);
+								// var newRow = `
+								// 	<tr id="officer-row-${response.id}">
+								// 		<td>${response.id}</td>
+								// 		<td>${response.lastname} ${response.firstname} ${response.middlename}</td>
+								// 		<td>${response.position}</td>
+								// 		<td><img src="${response.image}" alt="Officer Image" class="img-thumbnail" width="50" height="50"></td>
+								// 		<td>
+								// 			<button class="btn btn-sm btn-danger" onclick="delete_officer(${response.id})">
+								// 				<i class="fa fa-trash"></i> Delete
+								// 			</button>
+								// 		</td>
+								// 	</tr>
+								// `;
 								$('#officers-table tbody').append(newRow); // Append to the table
 							});
 						} else {
@@ -508,15 +506,23 @@
         }
     });
 });
-function previewOfficerImages(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#officer-images-preview').html('<img src="' + e.target.result + '" class="img-thumbnail" style="width: 100px; height: 100px;">');
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+		function previewOfficerImages(input) {
+			const previewContainer = $('#officer-images-preview');
+			previewContainer.html(''); // Clear previous previews
+			if (input.files) {
+				Array.from(input.files).forEach((file) => {
+					const reader = new FileReader();
+					reader.onload = function (e) {
+						const img = $('<img>')
+							.attr('src', e.target.result)
+							.css({ width: '100px', height: '100px', objectFit: 'cover', margin: '5px' })
+							.addClass('img-thumbnail');
+						previewContainer.append(img);
+					};
+					reader.readAsDataURL(file);
+				});
+			}
+		}
 		// end of officers code
 
 		function displayImg(input,_this) {
