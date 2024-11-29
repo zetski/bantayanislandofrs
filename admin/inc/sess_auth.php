4eaebce7bf9876e1e09f765f6351ae1ad8c74e95
zetski <batuigasjerichodesu@gmail.com>
2024-11-29 13:49:53 +0800

    sae

--- admin/inc/sess_auth.php
+++ admin/inc/sess_auth.php
@@ -12,14 +12,14 @@ else
 $link .= "://"; 
 $link .= $_SERVER['HTTP_HOST']; 
 $link .= $_SERVER['REQUEST_URI'];
-if(!isset($_SESSION['userdata']) && !strpos($link, 'login.php')){
+if(!isset($_SESSION['userdata']) && !strpos($link, 'login')){
 	redirect('admin/login.php');
 }
 if(isset($_SESSION['userdata']) && strpos($link, 'login.php')){
 	redirect('admin/index.php');
 }
 $module = array('','admin','tutor');
-if(isset($_SESSION['userdata']) && (strpos($link, 'index.php') || strpos($link, 'admin/login')) && $_SESSION['userdata']['login_type'] !=  1){
+if(isset($_SESSION['userdata']) && (strpos($link, 'index.php') || strpos($link, 'admin/')) && $_SESSION['userdata']['login_type'] !=  1){
 	echo "<script>alert('Access Denied!');location.replace('".base_url.$module[$_SESSION['userdata']['login_type']]."');</script>";
     exit;
 }
