1711e52d2a449783d849d772ff5eb20eb867770b
zetski <batuigasjerichodesu@gmail.com>
2024-11-30 07:05:47 +0800

    save

--- choose_access.php
+++ choose_access.php
@@ -56,7 +56,7 @@ if (isset($_SESSION['role'])) {
     <div class="container">
         <h2>Welcome to the Bantayan BFP</h2>
         <button class="btn guest-btn" onclick="window.location.href='./set_guest'">Continue as Guest</button>
-        <button class="btn admin-btn" onclick="window.location.href='../send_otp'">Login as Admin</button>
+        <button class="btn admin-btn" onclick="window.location.href='./verifyotp/send_otp'">Login as Admin</button>
     </div>
 </body>
 </html>
