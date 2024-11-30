9092ba9fccfe66492d197305d15b7b772ee6622f
zetski <batuigasjerichodesu@gmail.com>
2024-11-30 09:02:05 +0800

    save

--- verifyotp/send_otp.php
+++ verifyotp/send_otp.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $mail->send();
             $_SESSION['otp_email'] = $email; // Save email in session
-            header("Location: ./verifyotp/verify_otp");
+            header("Location: " . base_url() . "./verifyotp/verify_otp.php");
             exit;
         } catch (Exception $e) {
             $error_message = "Error sending OTP: {$mail->ErrorInfo}";
