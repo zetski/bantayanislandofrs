8ec126d58c79e9ea59cce4d81ac626a928038759
zetski <batuigasjerichodesu@gmail.com>
2024-11-30 11:22:31 +0800

    save

--- verifyotp/verify_otp.php
+++ verifyotp/verify_otp.php
@@ -103,7 +103,7 @@ ob_end_flush();
             text-align: left;
         }
 
-        input[type="number"] {
+        input[type="text"] {
             padding: 10px 15px;
             font-size: 14px;
             border: 1px solid #cccccc;
@@ -145,7 +145,7 @@ ob_end_flush();
         <p>Enter the one-time password (OTP) sent to your registered email address.</p>
         <form method="POST">
             <label for="otp">Enter OTP</label>
-            <input type="number" name="otp" id="otp" placeholder="Enter your 6-digit OTP" max-length="6" required>
+            <input type="text" name="otp" id="otp" placeholder="Enter your 6-digit OTP" required>
             <button type="submit">Verify OTP</button>
         </form>
     </div>
