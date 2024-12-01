51e6ff6fe1c8e1cb08e79859041b67d35d79ac01
zetski <batuigasjerichodesu@gmail.com>
2024-12-02 06:33:45 +0800

    save

--- admin/login.php
+++ admin/login.php
@@ -185,7 +185,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <div class="g-recaptcha" data-sitekey="6Lc_f4AqAAAAAP79JvQbC6_KbdOJQt9TRXxabqP3" data-callback="enableRecaptcha"></div>
           <div class="row">
             <div class="col-8">
-              <a href="forgot/forgot-password" style="display: inline-block; margin-top: 5px;" disabled>Forgot password?</a>
+              <a href="forgot/forgot-password.php" style="display: inline-block; margin-top: 5px;" disabled>Forgot password?</a>
             </div>
             <div class="col-4">
               <button type="submit" class="btn btn-primary btn-block" disabled>Sign In</button>
