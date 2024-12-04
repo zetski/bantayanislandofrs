0032a4c0becd94eea7c8e8cd783548a63fcd68d3
zetski <batuigasjerichodesu@gmail.com>
2024-12-04 14:05:58 +0800

    save

--- admin/inc/topBarNav.php
+++ admin/inc/topBarNav.php
@@ -64,9 +64,9 @@ $new_reports_count = $new_reports_query->num_rows;
 <nav class="main-header navbar navbar-expand navbar-light shadow text-sm">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
-    <li class="nav-item">
+    <!-- <li class="nav-item">
       <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
+    </li> -->
     <li class="nav-item d-none d-sm-inline-block">
       <a href="<?php echo base_url ?>" class="nav-link">
         <?php echo (!isMobileDevice()) ? $_settings->info('name') : $_settings->info('short_name'); ?> - Admin
