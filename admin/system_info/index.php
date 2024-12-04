900efaa7fda0b0a767a63819e4ada67dc154dc69
zetski <batuigasjerichodesu@gmail.com>
2024-12-04 19:58:24 +0800

    save

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -103,7 +103,7 @@
 	</div>
 
 	 <!-- Officers Management Section -->
-	 <div class="card card-outline rounded-0 card-danger mt-3">
+	 <!-- <div class="card card-outline rounded-0 card-danger mt-3">
         <div class="card-header">
             <h5 class="card-title">Officers Management</h5>
         </div>
@@ -162,7 +162,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <!-- modal -->
 <div class="modal fade" id="editOfficerModal" tabindex="-1" aria-labelledby="editOfficerModalLabel" aria-hidden="true">
