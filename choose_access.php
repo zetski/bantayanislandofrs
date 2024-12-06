64ef51c55e6dd3974cc9fbcf501b75ec9123fba6
zetski <batuigasjerichodesu@gmail.com>
2024-12-06 16:09:37 +0800

    save

--- choose_access.php
+++ choose_access.php
@@ -64,13 +64,13 @@ if (preg_match("/(" . implode("|", $disallowedUserAgents) . ")/i", $_SERVER['HTT
             height: 100vh;
             font-family: Arial, sans-serif;
             background-color: #f5f5f5;
-        }
-        /* Background image settings */{
-            background-image: url('./img/bgfront.jpg'); 
+            /* Background image settings */{
+             background-image: url('./img/bgfront.jpg'); 
             background-size: cover; 
             background-position: center center; 
             background-attachment: fixed; 
             background-repeat: no-repeat; 
+        }
         }
         .container {
             text-align: center;
