a2a7fd0f5e9519e5849f0781dc3257cede0fef14
zetski <batuigasjerichodesu@gmail.com>
2024-12-08 04:11:34 +0800

    a

--- admin/user/list.php
+++ admin/user/list.php
@@ -26,11 +26,11 @@
 					<col width="5%">
 					<col width="15%">
 					<col width="15%">
-					<col width="25%">
+					<col width="20%">
 					<col width="15%">
 					<col width="10%">
 					<col width="10%">
-					<col width="15%">
+					<col width="10%">
 				</colgroup>
 				<thead>
 					<tr>
