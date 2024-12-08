737a106f72aa7c22cb47b469f86b7a880320c0dd
zetski <batuigasjerichodesu@gmail.com>
2024-12-09 07:11:37 +0800

    save

--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,12 @@
 <?php 
-// session_start(); // Start session at the beginning
-// if (!isset($_SESSION['otp_verified']) || $_SESSION['otp_verified'] !== true) {
-//     echo "<script>
-//         alert('OTP not verified. Please verify it first.');
-//         window.location.href = 'https://bantayan-bfp.com/verifyotp/send_otp';
-//     </script>";
-//     exit;
-// }
+session_start(); // Start session at the beginning
+if (!isset($_SESSION['otp_verified']) || $_SESSION['otp_verified'] !== true) {
+    echo "<script>
+        alert('OTP not verified. Please verify it first.');
+        window.location.href = 'https://bantayan-bfp.com/verifyotp/send_otp';
+    </script>";
+    exit;
+}
 require_once('../config.php'); 
 
 // // Allowed IP addresses
