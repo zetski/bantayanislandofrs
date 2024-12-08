ccf976ae1c87e7155435c55bfb1028f196b62bac
zetski <batuigasjerichodesu@gmail.com>
2024-12-08 20:19:23 +0800

    save

--- citizencharter.php
+++ citizencharter.php
@@ -59,7 +59,7 @@ if (strpos($request, '.php') !== false) {
 
         .content {
             text-align: center;
-            padding: 20px;
+            padding-top: 20px;
             color: #fff;
         }
 
