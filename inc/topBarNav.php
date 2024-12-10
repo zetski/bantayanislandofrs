27841bcdd0a4d104f8b4ea6f9705401d705bb72c
zetski <batuigasjerichodesu@gmail.com>
2024-12-10 22:10:14 +0800

    s

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -6,9 +6,6 @@
   <title>Bantayan Fire Station</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <style>
-    .navbar-brand{
-      align-items: center;
-    }
     .navbar-brand img {
       border-radius: 50%;
     }
