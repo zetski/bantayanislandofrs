8af6c94751fba9feba2c182b642fb6320476c39b
zetski <batuigasjerichodesu@gmail.com>
2024-12-10 10:15:06 +0800

    s

--- inc/footer.php
+++ inc/footer.php
@@ -59,7 +59,7 @@
   })
 </script>
 <!-- Footer-->
-<footer class="py-3 bg-gradient-danger">
+<footer class="py-3 lg-expand bg-gradient-danger">
             <div class="container">
               <p class="m-0 text-center text-white"></?php echo $_settings->info('short_name') ?> 2024</p>
                <p class="m-0 text-center text-white">Developed By: <a href="mailto:oretnom23@gmail.com">oretnom23</a></p>
