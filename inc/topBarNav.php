352d8b61391e6aa0d57e75961aae974693c9ef20
zetski <batuigasjerichodesu@gmail.com>
2024-12-10 22:00:05 +0800

    s

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -131,10 +131,11 @@ session_start();
 
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #ff4600;">
   <div class="container px-4 px-lg-5">
-    <a class="navbar-brand" href="./">
+   <center><a class="navbar-brand" href="./">
       <img src="../img/r7logo.png" width="30" height="30" alt="Brand Logo" loading="lazy">
       Bantayan Fire Station
     </a>
+    </center> 
     <button class="navbar-toggler btn btn-sm" type="button" id="sidebarToggle" style="background-color: transparent !important; margin-left: 10px; border: none; padding-right: 10px; padding-top: 11px;">
       <span class="navbar-toggler-icon"></span>
     </button>
