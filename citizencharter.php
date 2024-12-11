8062dc91cfbfe1e63fe73ecfa41fc5f31ce51a6f
whoamijericho <whoamijericho@gmail.com>
2024-12-11 12:36:17 +0800

    s

--- citizencharter.php
+++ citizencharter.php
@@ -270,7 +270,7 @@ if (strpos($request, '.php') !== false) {
 
         <div class="video-container">
             <video controls>
-                <img src="img/firebg.jpg">
+            <source src="vid/vid.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
         </div>
